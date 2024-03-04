272712c8e78dd6b91b660be8fcf6bdc3dc748014
johnsamuel20 <joe.samuel.john.js@gmail.com>
2024-03-04 18:02:02 +0200

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   // base: '/SemiColon-registration-frontend',
   plugins: [react()],
+  base: "/SemiColon-registration-frontend/"
 })
